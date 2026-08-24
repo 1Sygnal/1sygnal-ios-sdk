991c92be322c4a1a6b8cd0eb60d0276a5597dfc3
ifiok <8256305+ifiok@users.noreply.github.com>
2026-08-24 10:32:05 +0000

    Release 1.0.1

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .binaryTarget(
             name: "OneSygnalSDK",
             url: "https://repo.1sygnal.app/ios/1.0.1/1Sygnal.zip",
-            checksum: "37dd476a91ef6d14a3e765f5e2b56430257ec8a84cb3a7b964a296ebb889ddbc"
+            checksum: "927a22021ec821a302b6eee1b646cd21eecfb8b4db88f8d266319f83b58e7eda"
         )
     ]
 )
